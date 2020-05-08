ed307ec2ef2ccebde352ede3e8811c00625119e7
AsanXu <2419770400@qq.com>
2020-05-08 12:42:45 +0800

    测试

---
+++ src/main/java/com/example/springbootdemo/dao/SysRoleDao.java
@@ -0,0 +1,66 @@
+package com.example.springbootdemo.dao;
+
+import com.example.springbootdemo.entity.SysRole;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+
+/**
+ * 角色信息表(SysRole)表数据库访问层
+ *
+ * @author makejava
+ * @since 2020-04-28 22:47:33
+ */
+@Mapper
+public interface SysRoleDao {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param roleId 主键
+     * @return 实例对象
+     */
+    SysRole queryById(Long roleId);
+
+    /**
+     * 查询指定行数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    List<SysRole> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
+
+    /**
+     * 通过实体作为筛选条件查询
+     *
+     * @param sysRole 实例对象
+     * @return 对象列表
+     */
+    List<SysRole> queryAll(SysRole sysRole);
+
+    /**
+     * 新增数据
+     *
+     * @param sysRole 实例对象
+     * @return 影响行数
+     */
+    int insert(SysRole sysRole);
+
+    /**
+     * 修改数据
+     *
+     * @param sysRole 实例对象
+     * @return 影响行数
+     */
+    int update(SysRole sysRole);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param roleId 主键
+     * @return 影响行数
+     */
+    int deleteById(Long roleId);
+
+}
